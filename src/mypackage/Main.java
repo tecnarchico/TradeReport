9f014ae27b700145cc463d5f236ee5f421e2e01d
tecnarchico <tecnarchico@github.com>
2018-03-26 06:10:48 +0200

    refactoring

--- src/mypackage/Main.java
+++ src/mypackage/Main.java
@@ -22,26 +22,28 @@ public class Main {
 	 * amount for a buy instruction, then foo is rank 1 for outgoing
 	 */
 	private final static DateFormat dateFormat = new SimpleDateFormat("dd/MMM/yyyy");
-	
-	
-	public static void main(String[] args) throws ParseException {
 
+	public final static String INCOMING = "incoming";
+	public final static String OUTGOING = "outgoing";
+	
+
+	public static void main(String[] args) {
+		List<Instruction> instructions;
+		try {
+			instructions = loadData();
+			elab(instructions);
+		} catch (ParseException e) {
+			System.out.println("There was an error parsing the data");
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static List<Instruction> loadData() throws ParseException {
+		
 		List<Instruction> instructions = new ArrayList<>();
 
-		instructions.add(new Instruction("foo", true, 0.50, Instruction.SGP, dateFormat.parse("01/Jan/2016"), dateFormat.parse("01/Jan/2016"), 200, 100.25));
-		instructions.add(new Instruction("bar", false, 0.22, Instruction.AED, dateFormat.parse("05/Jan/2016"), dateFormat.parse("07/Jan/2016"), 450, 150.50));
-		instructions.add(new Instruction("baz", true, 0.50, Instruction.SGP, dateFormat.parse("02/Jan/2016"), dateFormat.parse("03/Jan/2016"), 34, 100.25));
-		instructions.add(new Instruction("bar", false, 0.22, Instruction.AED, dateFormat.parse("06/Jan/2016"), dateFormat.parse("07/Jan/2016"), 42, 150.50));
-		instructions.add(new Instruction("foo", true, 0.50, Instruction.SGP, dateFormat.parse("04/Jan/2016"), dateFormat.parse("05/Jan/2016"), 34, 100.25));
-		instructions.add(new Instruction("baz", false, 0.22, Instruction.AED, dateFormat.parse("09/Jan/2016"), dateFormat.parse("10/Jan/2016"), 75, 150.50));
-		instructions.add(new Instruction("foo", true, 0.50, Instruction.SGP, dateFormat.parse("10/Jan/2016"), dateFormat.parse("11/Jan/2016"), 1200, 100.25));
-		instructions.add(new Instruction("bar", false, 0.22, Instruction.AED, dateFormat.parse("12/Jan/2016"), dateFormat.parse("13/Jan/2016"), 70, 150.50));
-		instructions.add(new Instruction("baz", true, 0.50, Instruction.SGP, dateFormat.parse("14/Jan/2016"), dateFormat.parse("15/Jan/2016"), 5500, 100.25));
-		instructions.add(new Instruction("bar", false, 0.22, Instruction.AED, dateFormat.parse("16/Jan/2016"), dateFormat.parse("16/Jan/2016"), 46, 150.50));
-		instructions.add(new Instruction("foo", true, 0.50, Instruction.SGP, dateFormat.parse("17/Jan/2016"), dateFormat.parse("18/Jan/2016"), 100, 100.25));
-		instructions.add(new Instruction("baz", false, 0.22, Instruction.AED, dateFormat.parse("20/Jan/2016"), dateFormat.parse("19/Jan/2016"), 87, 150.50));
-		
-	
 		//Expected output:
 		//
 		//		=============== Amount in USD settled incoming everyday
@@ -67,11 +69,23 @@ public class Main {
 		//		rank, entity, amount
 		//		1, bar, 20130.88
 		//		2, baz, 5363.82
+		instructions.add(new Instruction("foo", true, 0.50, Instruction.SGP, dateFormat.parse("01/Jan/2016"), dateFormat.parse("01/Jan/2016"), 200, 100.25));
+		instructions.add(new Instruction("bar", false, 0.22, Instruction.AED, dateFormat.parse("05/Jan/2016"), dateFormat.parse("07/Jan/2016"), 450, 150.50));
+		instructions.add(new Instruction("baz", true, 0.50, Instruction.SGP, dateFormat.parse("02/Jan/2016"), dateFormat.parse("03/Jan/2016"), 34, 100.25));
+		instructions.add(new Instruction("bar", false, 0.22, Instruction.AED, dateFormat.parse("06/Jan/2016"), dateFormat.parse("07/Jan/2016"), 42, 150.50));
+		instructions.add(new Instruction("foo", true, 0.50, Instruction.SGP, dateFormat.parse("04/Jan/2016"), dateFormat.parse("05/Jan/2016"), 34, 100.25));
+		instructions.add(new Instruction("baz", false, 0.22, Instruction.AED, dateFormat.parse("09/Jan/2016"), dateFormat.parse("10/Jan/2016"), 75, 150.50));
+		instructions.add(new Instruction("foo", true, 0.50, Instruction.SGP, dateFormat.parse("10/Jan/2016"), dateFormat.parse("11/Jan/2016"), 1200, 100.25));
+		instructions.add(new Instruction("bar", false, 0.22, Instruction.AED, dateFormat.parse("12/Jan/2016"), dateFormat.parse("13/Jan/2016"), 70, 150.50));
+		instructions.add(new Instruction("baz", true, 0.50, Instruction.SGP, dateFormat.parse("14/Jan/2016"), dateFormat.parse("15/Jan/2016"), 5500, 100.25));
+		instructions.add(new Instruction("bar", false, 0.22, Instruction.AED, dateFormat.parse("16/Jan/2016"), dateFormat.parse("16/Jan/2016"), 46, 150.50));
+		instructions.add(new Instruction("foo", true, 0.50, Instruction.SGP, dateFormat.parse("17/Jan/2016"), dateFormat.parse("18/Jan/2016"), 100, 100.25));
+		instructions.add(new Instruction("baz", false, 0.22, Instruction.AED, dateFormat.parse("20/Jan/2016"), dateFormat.parse("19/Jan/2016"), 87, 150.50));
 		
-		elab(instructions);
-	}
+		return instructions;
+	} 
 	
-	public static void elab(List<Instruction> instructions) {
+	private static void elab(List<Instruction> instructions) {
 		Map<Date, Double> settledIncomingByDate = new HashMap<>();
 		Map<Date, Double> settledOutgoingByDate = new HashMap<>();
 		Map<String, Double> settledIncomingByEntity = new HashMap<>();
@@ -120,49 +134,57 @@ public class Main {
 				settledOutgoingByEntity.put(entity, totAmountByEntity+amount);
 			}
 		}
-		
 
-		amount = 0.0;
-		
-		System.out.println("=============== Amount in USD settled incoming everyday");
+		printSettledIncomingByDate(settledIncomingByDate);
+		printSettledOutgoingByDatee(settledOutgoingByDate);
+		printSettledIncomingByEntity(settledIncomingByEntity);
+		printSettledOutgoingByEntity(settledOutgoingByEntity);
+	}
+
+
+	private static void printSettledIncomingByDate(Map<Date, Double> settledIncomingByDate) {
+		printSettledByDatee(settledIncomingByDate, INCOMING);
+	}
+	
+	private static void printSettledOutgoingByDatee(Map<Date, Double> settledOutgoingByDate) {
+		printSettledByDatee(settledOutgoingByDate, OUTGOING);
+	}
+	
+	private static void printSettledByDatee(Map<Date, Double> settledByDate, String direction) {
+		double amount = 0.0;
+		System.out.println(String.format("=============== Amount in USD settled %s everyday", direction));
 		System.out.println(String.format("%s, %s", "date", "amount"));		
-		settledIncomingByDate = new TreeMap<>(settledIncomingByDate);
-		for (Date date : settledIncomingByDate.keySet()) {
-			amount = settledIncomingByDate.get(date);
+		settledByDate = new TreeMap<>(settledByDate);
+		for (Date date : settledByDate.keySet()) {
+			amount = settledByDate.get(date);
 			System.out.println(String.format("%s, %s", dateFormat.format(date), amount));
 		}
-		
-		System.out.println("=============== Amount in USD settled outgoing everyday");
-		System.out.println(String.format("%s, %s", "date", "amount"));		
-		settledOutgoingByDate = new TreeMap<>(settledOutgoingByDate);
-		for (Date date : settledOutgoingByDate.keySet()) {
-			amount = settledOutgoingByDate.get(date);
-			System.out.println(String.format("%s, %s", dateFormat.format(date), amount));
-		}
-		
+	}
+	
+	private static void printSettledIncomingByEntity(Map<String, Double> settledIncomingByEntity) {
+		printSettledByEntity(settledIncomingByEntity, INCOMING);
+	}
+	
+	private static void printSettledOutgoingByEntity(Map<String, Double> settledOutgoingByEntity) {
+		printSettledByEntity(settledOutgoingByEntity, OUTGOING);
+	}
+	
+	private static void printSettledByEntity(Map<String, Double> settledByEntity, String direction) {
+		double amount = 0.0;
 		int rank = 0;
-		System.out.println("=============== Ranking of entities based on incoming amount");
+		System.out.println(String.format("=============== Ranking of entities based on %s amount", direction));
 		System.out.println(String.format("%s, %s, %s", "rank", "entity", "amount"));		
-		settledIncomingByEntity = sortByValue(settledIncomingByEntity, false);
-		for (String _entity : settledIncomingByEntity.keySet()) {
+		settledByEntity = sortByValue(settledByEntity, false);
+		for (String _entity : settledByEntity.keySet()) {
 			++rank;
-			amount = settledIncomingByEntity.get(_entity);
-			System.out.println(String.format("%s, %s, %s", rank, _entity, amount));
-		}
-		
-		rank = 0;
-		System.out.println("=============== Ranking of entities based on outgoing amount");
-		System.out.println(String.format("%s, %s, %s", "rank", "entity", "amount"));		
-		settledOutgoingByEntity = sortByValue(settledOutgoingByEntity, false);
-		for (String _entity : settledOutgoingByEntity.keySet()) {
-			++rank;
-			amount = settledOutgoingByEntity.get(_entity);
+			amount = settledByEntity.get(_entity);
 			System.out.println(String.format("%s, %s, %s", rank, _entity, amount));
 		}
 	}
-
+	
+	
 	// https://stackoverflow.com/a/2581754
-    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map, boolean asc) {
+	private static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map, boolean asc) {
         List<Entry<K, V>> list = new ArrayList<>(map.entrySet());
         if (asc) list.sort((o1, o2) -> o1.getValue().compareTo(o2.getValue()));
         else list.sort((o1, o2) -> - o1.getValue().compareTo(o2.getValue()));
@@ -175,5 +197,6 @@ public class Main {
 
         return result;
     }
+	
 
 }
